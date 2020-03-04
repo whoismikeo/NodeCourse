a6703554981e1383c6f214fe79c4d25cda83232a
Mike O <whoismikeo@gmail.com>
2020-03-03 18:57:00 -0800

    nice nodes u have there

--- Server.js
+++ Server.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Wow!</title></head>');
+    res.write('<body><h1>Bruh we really out here.</h1></body>');
+    res.write('</html');
+    res.end();
 });
 
 server.listen(3000);

5ff874cc262d025c306bf2ffbbbcbb3ccca040f4
Mike O <whoismikeo@gmail.com>
2020-03-07 19:41:38 -0800

    fuggit

--- Server.js
+++ Server.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
-const routes = require('./routes.js');
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Heyoooo');
+    next(); // Allows the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+    console.log('It\'s ya boi!');
+    res.send('<h1>Hot Damn!</h1>'); // Allows us to attach a body of any type (including a file)
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);

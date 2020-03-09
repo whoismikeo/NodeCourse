be8e2f0630f4ff87e854c4226fe99b6fbece8eb8
Mike O <whoismikeo@gmail.com>
2020-03-09 13:39:18 -0700

    wowie

--- Server.js
+++ Server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 
+const parser = require('body-parser');
+
 const express = require('express');
 
 const app = express();
+
+app.use(parser.urlencoded({extended: false})); // Parser. npm install --save body-parser
 
 app.use('/', (req, res, next) => {
     console.log('Always runnin boiii');
@@ -11,7 +15,12 @@ app.use('/', (req, res, next) => {
 
 app.use('/some-path', (req, res, next) => {
     console.log('It\'s ya boi!');
-    res.send('<h1>This is some path, eh?</h1>'); // Allows us to attach a body of any type (including a file)
+    res.send('<form action="/thing" method="POST"><input type="text" name="title"><button>Add Something</button></form>'); // Allows us to attach a body of any type (including a file)
+});
+
+app.use('/thing', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {

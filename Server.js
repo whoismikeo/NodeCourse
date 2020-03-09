df3dcafaee7f38c2069282b894af24118a986792
Mike O <whoismikeo@gmail.com>
2020-03-09 12:43:42 -0700

    tickle that pickle

--- Server.js
+++ Server.js
@@ -4,12 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Heyoooo');
-    next(); // Allows the request to continue to the next middleware
+app.use('/', (req, res, next) => {
+    console.log('Always runnin boiii');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/some-path', (req, res, next) => {
+    console.log('It\'s ya boi!');
+    res.send('<h1>This is some path, eh?</h1>'); // Allows us to attach a body of any type (including a file)
+});
+
+app.use('/', (req, res, next) => {
     console.log('It\'s ya boi!');
     res.send('<h1>Hot Damn!</h1>'); // Allows us to attach a body of any type (including a file)
 });
